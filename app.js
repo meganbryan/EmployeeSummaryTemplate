75a4fe1f2d535e46635ef604f75b257b1baa60e8
Megan Bryan <meganmbryan12@gmail.com>
2021-01-18 19:36:21 -0800

    Add inquirer functionality

--- app.js
+++ app.js
@@ -10,6 +10,149 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+const employeeList = []
+
+const employeeQuestions = [
+    { 
+        type: 'input',
+        message: 'What is their name?',
+        name: 'name'
+    },
+    { 
+        type: 'input',
+        message: 'What is their ID number?',
+        name: 'id'
+    },
+    { 
+        type: 'input',
+        message: 'What is their email address?',
+        name: 'email'
+    },
+    { 
+        type: 'list',
+        message: 'What is their employment type?',
+        choices: ["Manager", "Engineer", "Intern"],
+        name: 'employmentType'
+    }
+]
+
+function init() {
+    console.log (employeeList)
+    inquirer
+    .prompt(
+        { 
+            type: 'list',
+            message: 'Would you like to add another employee?',
+            choices: ["Yes", "No"],
+            name: 'addEmployee'
+        }
+    )
+    .then((answers) => {
+        if (answers.addEmployee === "Yes") {
+            chooseType ();
+        }
+        else {
+            const renderEmployees = render(employeeList)
+            fs.writeToFile(outputPath, renderEmployees);
+        }
+    })
+    .catch(error => {
+        if(error) {
+            console.error(error)
+        }
+    })
+}
+
+function chooseType () {
+    inquirer
+    .prompt(employeeQuestions)
+    .then((answers) => {
+        if (answers.employmentType === "Manager") {
+            addManager();
+        }
+        else if (answers.employmentType === "Engineer") {
+            addEngineer();
+        }
+        else if (answers.employmentType === "Intern") {
+            addIntern();
+        }
+        else {
+            console.log ("error")
+        }
+    })
+    .catch(error => {
+        if(error) {
+            console.error(error)
+        }
+    })
+}
+
+function addManager () {
+    inquirer
+    .prompt(
+        {
+            type: 'input',
+            message: 'What is their office number?',
+            name: 'officeNumber'
+        }
+    )
+    .then((answers) => {
+        const newManager = Manager (answers)
+        employeeList.push(newManager)
+        init ();
+    })
+    .catch(error => {
+        if(error) {
+            console.error(error)
+        }
+    })
+}
+
+function addEngineer () {
+    inquirer
+    .prompt(
+        {
+            type: 'input',
+            message: 'What is their GitHub URL?',
+            name: 'github'
+        }
+    )
+    .then((answers) => {
+        const newEngineer = Engineer (answers);
+        employeeList.push(newEngineer)
+        init ();
+    })
+    .catch(error => {
+        if(error) {
+            console.error(error)
+        }
+    })
+}
+
+function addIntern () {
+    inquirer
+    .prompt(
+        {
+            type: 'input',
+            message: 'What school are they attending?',
+            name: 'school'
+        }
+    )
+    .then((answers) => {
+        const newIntern = Intern (answers);
+        employeeList.push(newIntern)
+        init ();
+    })
+    .catch(error => {
+        if(error) {
+            console.error(error)
+        }
+    })
+}
+
+
+// function call to initialize program
+init();
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
